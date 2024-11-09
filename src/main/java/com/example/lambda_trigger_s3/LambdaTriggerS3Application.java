dc8586f600fd9eb8e7abd691af8ed767c5880346
prasac <prasac@localhost.com>
2024-11-09 15:33:41 +0700

    1st commit

---
+++ src/main/java/com/example/lambda_trigger_s3/LambdaTriggerS3Application.java
@@ -0,0 +1,13 @@
+package com.example.lambda_trigger_s3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LambdaTriggerS3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LambdaTriggerS3Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/lambda_trigger_s3/LambdaTriggerS3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.lambda_trigger_s3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LambdaTriggerS3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
